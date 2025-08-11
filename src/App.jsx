40bd868366f389b8761b59b1f5effd42a5e6ede6
KIJ <dlrwn0730@naver.com>
2025-08-11 15:43:12 +0900

    router2

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import About from './pages/About';
+import Board from './pages/Board';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import BoardDetail from './pages/BoardDetail';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/board" element={<Board />} />
+        <Route path="/board/:id" element={<BoardDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Nav.css'
+
+const Nav = () => {
+    return (
+        <nav>
+            <h1><NavLink to="/">Title</NavLink></h1>
+            <ul className="nav-list">
+                <li><NavLink to="/">홈</NavLink></li>
+                <li><NavLink to="/about">소개</NavLink></li>
+                <li><NavLink to="/board">게시판리스트</NavLink></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
